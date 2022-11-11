891018ffbbde04a34d277bf383462bfb72a929ec
Oleksandr Vorobei <oleksandr.vorobei@anvileight.com>
2022-11-11 14:02:55 +0200

    Add lecture materials

---
+++ lections/test_python/main.py
@@ -0,0 +1,11 @@
+def numbers(a: int) -> str:
+    return {0: "zero", 1: "one", 2: "two", 3: "three", 4: "four"}.get(a, "unexpected")
+
+
+class Taras:
+    def __init__(self, age, sex):
+        self.age = age
+        self.sex = sex
+
+    def question(self):
+        return "А виконання команди make покажи"
